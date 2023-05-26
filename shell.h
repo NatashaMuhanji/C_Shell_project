37400db83a7c426f85f0cef9ec6dfc96ce09c234
NatashaMuhanji <natashamuhanji@gmail.com>
2023-05-26 13:34:55 -0700

    shell files

--- shell.h
+++ shell.h
@@ -1,114 +1,30 @@
-#include "shell.h"
+#ifndef SHELL_H
+#define SHELL_H
 
-<<<<<<< HEAD
-/*
- * int line_number = 1, status = 0;
- * char *sname = NULL;
- * main - main is used to execute commands
- * @ac: these are the number of inputs
- * array of input
- * Return: 0, or anothe rnumber
-{
-	int read_bytes, separated = FALSE, a;
-	size_t buffer_size = 1;
-	char *buffer = NULL, *buffer_ptr, *buffer_tmp;
-	char **arguments = NULL;
-=======
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <sys/types.h>
+#include <errno.h>
 #include <stddef.h>
 #include <sys/stat.h>
 #include <signal.h>
-#include <errno.h>
-#include <stdlib.h>
-#include <sys/types.h>
->>>>>>> 45b61f670ea4a90594807eb394a7b0fe2d9f1eb1
 
-	sname = strdup(*av);
+int _putchar(char c);
+void _puts(char *str);
+int _strlen(char *s);
+char *_strdup(char *str);
+char *concat_all(char *name, char *sep, char *value);
 
-<<<<<<< HEAD
-	environ = array_cpy(environ, list_len(environ, NULL));
-=======
 char **splitstring(char *str, const char *delim);
 void _execute_(char **argv);
 void *_realloc_(void *ptr, unsigned int old_size, unsigned int new_size);
 
->>>>>>> 45b61f670ea4a90594807eb394a7b0fe2d9f1eb1
 
-	signal(SIGINT, SIG_IGN);
+extern char **environ;
 
-<<<<<<< HEAD
-	buffer = malloc(1);
-	if (buffer == NULL)
-		exit(EXIT_FAILURE);
-
-	while (1)
-	{
-		if (separated == FALSE)
-		{
-			if (isatty(STDIN_FILENO) == 1)
-				write(STDOUT_FILENO, "my_shell$ ", 10);
-
-			read_bytes = getline(&buffer, &buffer_size, stdin);
-
-			if (read_bytes == -1)
-				break;
-			if (read_bytes == 1)
-			{
-				line_number++;
-				continue;
-			}
-			buffer[read_bytes - 1] = '\0';
-			buffer = input_san(buffer, &buffer_size);
-			if (buffer_size == 0)
-			{
-				line_number++;
-				continue;
-			}
-			buffer_ptr = buffer;
-		}
-		else
-			buffer_ptr = buffer_tmp;
-
-		buffer_tmp = NULL;
-		arguments = make_array(buffer_ptr, ' ', &buffer_tmp);
-		if (buffer_tmp != NULL)
-			separated = TRUE;
-		else
-			separated = FALSE;
-
-		a = command_manager(arguments);
-
-		free(arguments);
-
-		if (separated == FALSE)
-			line_number++;
-
-		if (a == EXIT_SHELL)
-			break;
-	}
-	free(buffer);
-	alias_func(NULL, TRUE);
-	free_array(environ);
-	free(sname);
-
-	return (status % 256);
-}
-#include "shell.h"
-*/
-/**
- * main - This is the shell.c file for our project
- * Return:Return 0 if success
- */
-int main(void)
-{
-	ssize_t bytes_rd = 0;
-	size_t bf_size = 0;
-	char *entry = NULL, *arguments[20];
-	int counter = 1, vf_stat = 0, exist_stat = 0, exit_stat = 0, blt_stat = 0;
-=======
 /**
  * struct _listpath - Linked list containing PATH directories
  * @dir: directory in path
@@ -149,41 +65,4 @@ void free_list(_listpath *head);
 
 
 #endif
->>>>>>> 45b61f670ea4a90594807eb394a7b0fe2d9f1eb1
 
-	_printp("$ ", 2);
-	bytes_rd = getline(&entry, &bf_size, stdin);
-	while (bytes_rd != -1)
-	{
-		if (*entry != '\n')
-		{
-			fill_args(entry, arguments);
-			if (arguments[0] != NULL)
-			{
-				exist_stat = exist(arguments[0]);
-				if (exist_stat != 0)
-				{
-					vf_stat = verify_path(arguments);
-					if (vf_stat == 0)
-						exit_stat = exec(arguments), free(entry), free(*arguments);
-					else
-					{
-					blt_stat = verify_blt(arguments, exit_stat);
-					if (blt_stat != 0)
-						exit_stat = print_not_found(arguments, counter), free(entry);
-					}
-				}
-				else
-					exit_stat = exec(arguments), free(entry);
-			}
-			else
-				free(entry);
-		}
-		else if (*entry == '\n')
-			free(entry);
-		entry = NULL, counter++;
-		_printp("$ ", 2), bytes_rd = getline(&entry, &bf_size, stdin);
-	}
-	last_free(entry);
-	return (exit_stat);
-}
